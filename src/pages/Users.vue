234a77006b8a960fa1a55076cbb561dea296c562
kristjanjansen <kristjan.jansen@gmail.com>
2021-03-09 11:38:11 +0200

    cleanup

--- src/pages/Users.vue
+++ src/pages/Users.vue
@@ -1,25 +1,12 @@
 <script setup>
 import { computed } from "vue";
-import { differenceInMilliseconds } from "date-fns";
-import { users, config, scale } from "../lib";
+import { users } from "../lib";
 
 const updatedUsers = computed(() =>
   users.value
     .map((user) => {
-      user.updatedSince = differenceInMilliseconds(
-        new Date(),
-        new Date(user.datetime)
-      );
-      user.opacity = scale(
-        Math.max(user.updatedSince, 10000),
-        0,
-        config.userUpdatedSinceLimit,
-        1,
-        0.1
-      );
       return user;
     })
-    .filter((user) => user.updatedSince < config.userUpdatedSinceLimit)
     .sort((a, b) => a.userId.localeCompare(b.userId))
 );
 </script>
