a102b1dba5b39f72963752c849e6fe671abd8ec8
kristjanjansen <kristjan.jansen@gmail.com>
2021-03-09 16:03:55 +0200

    test

--- src/pages/Threesixty.vue
+++ src/pages/Threesixty.vue
@@ -1,8 +1,26 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
+import { Video } from "@thiagopnts/kaleidoscope";
+
 import { useVideoStream, config, replace } from "../lib";
-const src = replace(config.streamUrl, { streamkey: "360" });
+
+// const src = replace(config.streamUrl, { streamkey: "360" });
+const src =
+  "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8";
 const { videoRef, status, width, height } = useVideoStream(src);
+
+const video360Ref = ref(null);
+
+onMounted(() => {
+  if (videoRef.value) {
+    var viewer = new Video({
+      source: videoRef.value,
+      container: video360Ref.value,
+    });
+    viewer.render();
+  }
+});
+
 const muted = ref(true);
 </script>
 
@@ -19,6 +37,7 @@ const muted = ref(true);
         aspectRatio: width + ' / ' + height,
       }"
     />
+    <div ref="video360Ref" class="debug" />
     <IconUnmute v-if="muted" @click="muted = !muted" />
     <IconMute v-if="!muted" @click="muted = !muted" />
   </div>
