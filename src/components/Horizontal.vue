b23af79eaf29601bdb46e4ce8005472ae9422fca
kristjanjansen <kristjan.jansen@gmail.com>
2021-03-18 09:33:25 +0200

    new components

---
+++ src/components/Horizontal.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Horizontal">
+    <slot />
+  </div>
+</template>
+
+<style>
+.Horizontal {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: clamp(8px, 5vw, 64px);
+}
+@media (max-width: 800px) {
+  .Horizontal {
+    grid-template-columns: 1fr;
+  }
+}
+</style>
