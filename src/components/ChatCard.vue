7d15c68fce5fd9d8109aacbfc7bb60b71873f366
kristjanjansen <kristjan.jansen@gmail.com>
2021-03-18 12:33:55 +0200

    hm

--- src/components/ChatCard.vue
+++ src/components/ChatCard.vue
@@ -3,12 +3,10 @@ defineProps({ chat: { type: Object } });
 </script>
 
 <template>
-  <div class="ChatCard">
+  <div>
     <div style="font-size: 0.8em; opacity: 0.5">
       {{ chat.userName }}
     </div>
-    <div>{{ chat.value }}</div>
+    <div style="font-size: 0.9em">{{ chat.value }}</div>
   </div>
 </template>
-
-<style scoped></style>
