ee83ea1f5163ad5c9ee83c3856d7b19290a65815
kristjanjansen <kristjan.jansen@gmail.com>
2021-03-19 16:43:45 +0200

    cleanup

--- src/lib/dom.js
+++ src/lib/dom.js
@@ -1,4 +1,12 @@
-export const useTextarea = (callback = () => {}) => {
+import {
+  nextTick,
+  onMounted,
+  onUnmounted,
+  ref,
+  watch,
+} from 'vue';
+
+export const useChatTextarea = (callback = () => {}) => {
   const el = ref(null);
 
   const onKeydown = (e) => {
@@ -27,8 +35,74 @@ export const useTextarea = (callback = () => {}) => {
   onUnmounted(() => {
     if (el.value) {
       el.value.removeEventListener("keydown", onKeydown);
+      el.value.removeEventListener("input", onInput);
     }
   });
+
+  return el;
+};
+
+export const useAboutTextarea = (visible) => {
+  const el = ref(null);
+
+  // const onKeydown = (e) => {
+  //   if (e.key === "Enter" && !e.shiftKey) {
+  //     e.preventDefault();
+  //     callback();
+  //     el.value.style.height = "auto";
+  //   }
+  // };
+
+  const onInput = (e) => {
+    // el.value.style.height = "auto";
+    // el.value.style.height = el.value.scrollHeight + "px";
+  };
+
+  // nextTick(() => {
+  //   console.log("a");
+  //   el.value.focus();
+  //   el.value.select();
+  // });
+
+  watch(
+    [() => el.value, () => visible.value],
+    () => {
+      if (el.value && visible.value) {
+        console.log(el.value, visible.value);
+        // el.value.focus();
+        // el.value.select();
+        nextTick(() => {
+          el.value.focus();
+          el.value.select();
+        });
+      }
+    },
+    { immediate: true }
+  );
+  /*
+  onMounted(() => {
+    if (el.value) {
+      //      nextTick(() => {
+      el.value.focus();
+      el.value.select();
+      //    });
+      // el.value.focus();
+      // el.value.select();
+      //el.value.focus();
+      //el.value.addEventListener("keydown", onKeydown);
+      // @TODO Run when event exists
+      //el.value.select();
+      //onInput();
+      //  el.value.addEventListener("input", onInput);
+    }
+  });
+*/
+  // onUnmounted(() => {
+  //   if (el.value) {
+  //     el.value.removeEventListener("keydown", onKeydown);
+  //     el.value.removeEventListener("input", onInput);
+  //   }
+  // });
 
   return el;
 };
