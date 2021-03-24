cb156680866afaccd3e3fafec518953db210537e
kristjanjansen <kristjan.jansen@gmail.com>
2021-03-24 13:30:39 +0200

    cleanup

--- src/lib/date.js
+++ src/lib/date.js
@@ -3,11 +3,13 @@ import { ref } from 'vue';
 
 import {
   compareDesc,
-  differenceInHours,
+  differenceInMinutes,
   format,
   formatDistanceStrict,
   intervalToDuration,
 } from 'date-fns';
+
+import { sentencecase } from './';
 
 const timezoneAbbr = (dateInput) => {
   let dateString = dateInput + "",
@@ -48,10 +50,6 @@ export const createDate2 = (dateStr, timeStr, tz = "+02:00") =>
     timeZone,
   });
 
-// .toLocaleString("et", {
-//   timeZone: "Europe/Tallinn",
-// });
-
 export const createNow = () => createDate(new Date());
 
 const isDatetime = (str) => String(str).match(/:/g);
@@ -70,11 +68,26 @@ export const formatDate = (str) => {
 
 export const sortEvents = (a, b) => compareDesc(b.fromDatetime, a.fromDatetime);
 
+const getDifferenceBetween = (fromDatetime, toDatetime) => {
+  const soonMinutes = 3 * 60;
+  const diffStart = differenceInMinutes(fromDatetime, now);
+  const diffEnd = differenceInMinutes(toDatetime, now);
+  if (diffStart < 0 && diffEnd >= 0) {
+    return { diff: "now", diffStart, diffEnd };
+  } else if (diffStart >= 0 && diffStart <= soonMinutes) {
+    return { diff: "soon", diffStart, diffEnd };
+  } else if (diffStart >= 0 && diffStart > soonMinutes) {
+    return { diff: "future", diffStart, diffEnd };
+  } else {
+    return { diff: "past", diffStart, diffEnd };
+  }
+};
+
 export const getDiff = (event) => {
   let { fromdate, fromtime, todate, totime } = event;
   fromtime = fromtime.trim() ? fromtime : "00:00";
   todate = todate.trim() ? todate : fromdate;
-  totime = totime.trim() ? totime : "00:00";
+  totime = totime.trim() ? totime : "24:00";
 
   // const tz = now.getTimezoneOffset() / -60;
   // const timezone = `${tz < 0 ? "-" : "+"}${String(tz).padStart(2, "0")}:00`;
@@ -98,15 +111,25 @@ export const getDiff = (event) => {
 
   const formattedToDatetime = formatDate(toDatetime);
 
-  const distance = formatDistanceStrict(fromDatetime, now, {
-    roundingMethod: "ceil",
-    addSuffix: true,
-  });
+  const formatDistance = (datetime, datetimeStatus = null) => {
+    const distance = sentencecase(
+      formatDistanceStrict(datetime, now, {
+        roundingMethod: "ceil",
+        addSuffix: true,
+      })
+    );
+    return datetimeStatus === "now" ? `Started ${distance}` : distance;
+  };
+
+  const diff2 = getDifferenceBetween(fromDatetime, toDatetime);
+
+  const distance = formatDistance(fromDatetime, diff2.diff);
 
   return {
     fromDatetime,
     toDatetime,
     diff: {
+      diff2,
       fromdate,
       fromtime,
       fromDatetime,
@@ -123,28 +146,28 @@ export const getDiff = (event) => {
 export const getDifference = (start, end = null) =>
   end ? getDifferenceBetween(start, end) : getDifferenceStart(start);
 
-const getDifferenceBetween = (start, end) => {
-  const diffStart = differenceInHours(start, createNow());
-  const diffEnd = differenceInHours(createDate(end), createNow());
-  if (isDatetime(start) && isDatetime(end)) {
-    if (diffStart < 0 && diffEnd >= 0) {
-      return { diff: "now", diffStart, diffEnd };
-    } else if (diffStart >= 0 && diffStart <= 3) {
-      return { diff: "soon", diffStart, diffEnd };
-    } else if (diffStart >= 0 && diffStart > 3) {
-      return { diff: "future", diffStart, diffEnd };
-    } else {
-      return { diff: "past", diffStart, diffEnd };
-    }
-  } else {
-    // No time specified
-    if (diffStart < 0) {
-      return { diff: "past", diffStart, diffEnd };
-    } else {
-      return { diff: "future", diffStart, diffEnd };
-    }
-  }
-};
+// const getDifferenceBetween = (start, end) => {
+//   const diffStart = differenceInHours(start, now);
+//   const diffEnd = differenceInHours(createDate(end), now);
+//   if (isDatetime(start) && isDatetime(end)) {
+//     if (diffStart < 0 && diffEnd >= 0) {
+//       return { diff: "now", diffStart, diffEnd };
+//     } else if (diffStart >= 0 && diffStart <= 3) {
+//       return { diff: "soon", diffStart, diffEnd };
+//     } else if (diffStart >= 0 && diffStart > 3) {
+//       return { diff: "future", diffStart, diffEnd };
+//     } else {
+//       return { diff: "past", diffStart, diffEnd };
+//     }
+//   } else {
+//     // No time specified
+//     if (diffStart < 0) {
+//       return { diff: "past", diffStart, diffEnd };
+//     } else {
+//       return { diff: "future", diffStart, diffEnd };
+//     }
+//   }
+// };
 
 const getDifferenceStart = (start) => {
   const diffStart = differenceInHours(start, createNow());
