42bc7e84fc99dd9e6a71e9962ebca7a66c54b2b8
kristjanjansen <kristjan.jansen@gmail.com>
2021-05-15 19:37:02 +0300

    work on dates

--- src/pages/Strapi.vue
+++ src/pages/Strapi.vue
@@ -1,18 +1,37 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import ky from "ky";
-import { compareDesc } from "date-fns";
+import { compareDesc, differenceInMinutes } from "date-fns";
+import { useNow } from "@vueuse/core";
 import { formatMarkdown, config } from "../lib";
 
 const strapi = ky.extend({
   prefixUrl: config.strapiUrl,
 });
 
+const { now } = useNow(1000 * 60); // Update in each minue
+
+const dateUrgency = (startAtDate, endAtDate) =>
+  computed(() => {
+    const soonMinutes = 3 * 60; // In 3 hours
+    const started = differenceInMinutes(startAtDate, now.value);
+    const ended = differenceInMinutes(endAtDate, now.value);
+    if (started < 0 && ended >= 0) {
+      return "now";
+    } else if (started >= 0 && started <= soonMinutes) {
+      return "soon";
+    } else if (started >= 0 && started > soonMinutes) {
+      return "future";
+    } else {
+      return "past";
+    }
+  });
+
 const processEvents = (event) => {
   if (event.description) {
     event.description = formatMarkdown(event.description_estonian);
   }
-  event.urgency = "now";
+  event.urgency = dateUrgency(new Date(event.start_at), new Date(event.end_at));
   return event;
 };
 
