3df0fcfa3ffe842fd47019701965dae1a1c32da9
kristjanjansen <kristjan.jansen@gmail.com>
2021-05-19 10:05:41 +0300

    cleanup

--- src/components/StrapiFienta.vue
+++ src/components/StrapiFienta.vue
@@ -4,8 +4,8 @@ import { useStorage } from "@vueuse/core";
 import { useTicket } from "../lib";
 
 const props = defineProps({
-  festival: { type: Object, default: {} },
-  event: { type: Object, default: {} },
+  festival: { type: Object },
+  event: { type: Object },
 });
 
 const tickets = useStorage("elektron_data", []);
@@ -28,35 +28,37 @@ const { status } = useTicket(festival, event);
 </script>
 
 <template>
-  <div v-if="status === 'FREE'" style="opacity: 0.3">Free event</div>
-  <flex
-    v-if="status !== 'FREE'"
-    style="gap: 16px"
-    :style="{
-      color:
-        status === 'HAS_FESTIVAL_TICKET' || status === 'HAS_EVENT_TICKET'
-          ? '#2ECC40'
-          : 'var(--ticket)',
-    }"
-  >
-    <icon-creditcard />
-    <div v-if="status === 'HAS_FESTIVAL_TICKET'">
-      You have a festival ticket
-    </div>
-    <div v-if="status === 'HAS_EVENT_TICKET'">You have an event ticket</div>
-    <a
-      v-if="status === 'REQUIRES_TICKET' && festival?.fienta_url"
-      :href="festival?.fienta_url"
-      target="_black"
+  <div v-if="festival && event">
+    <div if="status === 'FREE'" style="opacity: 0.3">Free event</div>
+    <flex
+      v-if="status !== 'FREE'"
+      style="gap: 16px"
+      :style="{
+        color:
+          status === 'HAS_FESTIVAL_TICKET' || status === 'HAS_EVENT_TICKET'
+            ? '#2ECC40'
+            : 'var(--ticket)',
+      }"
     >
-      Get the festival ticket
-    </a>
-    <a
-      v-if="status === 'REQUIRES_TICKET' && event?.fienta_url"
-      :href="event?.fienta_url"
-      target="_black"
-    >
-      Get the event ticket
-    </a>
-  </flex>
+      <icon-creditcard />
+      <div v-if="status === 'HAS_FESTIVAL_TICKET'">
+        You have a festival ticket
+      </div>
+      <div v-if="status === 'HAS_EVENT_TICKET'">You have an event ticket</div>
+      <a
+        v-if="status === 'REQUIRES_TICKET' && festival?.fienta_url"
+        :href="festival?.fienta_url"
+        target="_black"
+      >
+        Get the festival ticket
+      </a>
+      <a
+        v-if="status === 'REQUIRES_TICKET' && event?.fienta_url"
+        :href="event?.fienta_url"
+        target="_black"
+      >
+        Get the event ticket
+      </a>
+    </flex>
+  </div>
 </template>
