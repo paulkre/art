d0f6b02e905b99ae6efb31415d856585e139f89b
kristjanjansen <kristjan.jansen@gmail.com>
2021-05-19 10:34:55 +0300

    clean

--- src/components/StrapiFestival.vue
+++ src/components/StrapiFestival.vue
@@ -9,9 +9,41 @@ const festivalRoute = computed(() => `/strapi/${props.festival.slug}`);
 </script>
 
 <template>
+  <router-link :to="festivalRoute" class="strapi-festival">
+    <vertical>
+      <h2>{{ festival.title }}</h2>
+    </vertical>
+  </router-link>
+</template>
+
+<style scoped>
+.strapi-festival {
+  display: grid;
+  aspect-ratio: 1;
+  background: red;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+</style>
+
+<!--
+<script setup>
+import { defineProps, computed } from "vue";
+
+const props = defineProps({
+  festival: { type: Object },
+});
+
+const festivalRoute = computed(() => `/strapi/${props.festival.slug}`);
+</script>
+
+<template>
   <router-link :to="festivalRoute">
     <vertical>
       <h2>{{ festival.title }}</h2>
     </vertical>
   </router-link>
 </template>
+-->
